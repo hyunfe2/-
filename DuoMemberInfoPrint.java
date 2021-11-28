6d7566c52ed0b7676c321da46a6b808a0284ac1c
우현철 <95002506+hyunfe2@users.noreply.github.com>
2021-11-28 18:17:45 +0900

    Add files via upload

---
+++ DuoMemberInfoPrint.java
@@ -0,0 +1,52 @@
+
+public class DuoMemberInfoPrint {
+
+		
+		public static void main(String[] args) {
+				
+	
+			
+			String stars = "*************************";
+			String a1 = "홍길동";
+			String a2 = "201212-3495039";
+			String a3 = "male";
+			
+			boolean bool1;
+			
+			bool1 = true;
+			
+			char  c1;
+			c1 = 'A';
+			
+			int n1 = 181818;
+			int n2 = 0;
+			
+			double n3 = 178.3;
+			
+			System.out.println(stars);
+		
+			System.out.println("회원정보 : " + n1);
+			
+			System.out.println("이   름 : " + a1);
+			
+			System.out.println("주민번호 : " + a2);
+
+			System.out.println("성   별 : " + a3);
+
+			System.out.println("결혼횟수 :  " +n2);
+
+			System.out.println("흡연여부 : " + bool1);
+
+			System.out.println(" 키    : " + n3 );
+
+			System.out.println(" 학점   : " + c1 );
+
+			System.out.println(stars);
+			
+			
+			 
+			
+
+	}
+
+}
